9433fd2806d579e12893de4ea7e498a6bd726325
JT <jtvinolus@gmail.com>
2022-05-20 18:20:27 -0700

    Testing

--- code/src/js/script.js
+++ code/src/js/script.js
@@ -9,3 +9,9 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+function signInHelp()
+{
+	return "Sign In"
+}
