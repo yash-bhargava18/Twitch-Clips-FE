e6d4b0b3cdd902d78fc5709e88fbcae61912f288
JT <jtvinolus@gmail.com>
2022-05-21 23:56:41 -0700

    New stuff

--- code/src/js/script.js
+++ code/src/js/script.js
@@ -2,6 +2,9 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+/* const signInButtonLogin = document.getElementById('SignInLogin'); */
+
+
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
@@ -9,3 +12,8 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+
+
