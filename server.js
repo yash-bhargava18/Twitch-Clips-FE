008cac945022a26c5438056dcfa1efe1e77d38ec
Yash Bhargava <98626961+yash-bhargava18@users.noreply.github.com>
2022-05-22 17:13:07 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+app.use(express.static(__dirname + '/src'));
+app.get("/", (req, res)=>{
+res.sendFile(path.join(__dirname, "..", "code","src", "index.html"));
+});
+
+
+
+app.post("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "code","src", "selection.html"))
+})
+
+
+
+app.listen(3000, (req, res)=>{
+console.log("Server has started ! ");
+})
